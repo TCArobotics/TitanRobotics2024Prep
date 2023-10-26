53a30b90f94ff54a146dfa27660c1b8376c98e9a
impossiblerocket <46904736+impossiblerocket@users.noreply.github.com>
2023-10-26 16:45:42 -0600

    Initialized encoders(most of them)

--- 2024/src/main/java/frc/robot/data/PositionEstimate.java
+++ 2024/src/main/java/frc/robot/data/PositionEstimate.java
@@ -1,5 +1,34 @@
 package frc.robot.data;
 
-public class PositionEstimate {
-    
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Encoder;
+
+public class PositionEstimate 
+{
+
+    //distance encoders(duh)
+    public static Encoder LEFT_FRONT_DISTANCE_ENCODER;
+    public static Encoder LEFT_REAR_DISTANCE_ENCODER;
+    public static Encoder RIGHT_FRONT_DISTANCE_ENCODER;
+    public static Encoder RIGHT_REAR_DISTANCE_ENCODER;
+
+    //direction encoders(duh)
+    public static Encoder LEFT_FRONT_DIRECTION_ENCODER;
+    public static Encoder LEFT_REAR_DIRECTION_ENCODER;
+    public static Encoder RIGHT_FRONT_DIRECTION_ENCODER;
+    public static Encoder RIGHT_REAR_DIRECTION_ENCODER;
+
+    public PositionEstimate()
+    {
+        
+
+        LEFT_FRONT_DIRECTION_ENCODER.setDistancePerPulse(360/1024); //THIS IS IN DEGREES, 360 degrees over 1024 counts per revolution
+        LEFT_REAR_DIRECTION_ENCODER.setDistancePerPulse(360/1024); 
+        RIGHT_FRONT_DIRECTION_ENCODER.setDistancePerPulse(360/1024);
+        RIGHT_REAR_DIRECTION_ENCODER.setDistancePerPulse(360/1024);
+    }
+
+
+
+
 }
