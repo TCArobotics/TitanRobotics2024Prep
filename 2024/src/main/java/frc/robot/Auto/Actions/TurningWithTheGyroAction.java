a92a0e1ba39c656e7f3ee289b49b265cc3119ba8
rsneller <rhea.sneller@gmail.com>
2023-10-26 16:49:09 -0600

    Added Modifiers
    Added some variables, and PID values.

---
+++ 2024/src/main/java/frc/robot/Auto/Actions/TurningWithTheGyroAction.java
@@ -0,0 +1,61 @@
+package frc.robot.Auto.Actions;
+//import frc.robot.Auto.Actions.Actions;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.sub.Drive;
+import frc.robot.sub.Gyro;
+
+public class TurningWithTheGyroAction implements Actions
+{   
+    Timer timer;
+    double angle;
+    double speed;
+    private Drive mDrive = null;
+    /**
+     * Run code once when the action is started, for setup
+     */
+    public TurningWithTheGyroAction(double angle, double speed)
+    {
+        this.angle = angle;
+        this.speed = speed;
+    }
+
+    @Override
+    public void start(){
+        angle = 0;
+        angle = (angle - getyah()) + getyah();
+       }
+
+    /**
+     * Called by runAction in AutoModeBase iteratively until isFinished returns true. Iterative logic lives in this
+     * method
+     */
+    @Override
+    public void update(){
+    mDrive.drive(this.speed,this.angle);
+    }
+
+    /**
+     * Returns whether or not the code has finished execution. When implementing this interface, this method is used by
+     * the runAction method every cycle to know when to stop running the action
+     *
+     * @return boolean
+     */
+    @Override
+    public boolean isFinished(){
+        if (timer.get() >= angle){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     * Run code once when the action finishes, usually for clean up
+     */
+    @Override
+    public void done(){
+        mDrive.drive(0, 0);
+    }
+
+}
---
+++ 2024/src/main/java/frc/robot/sub/Modifiers.java
@@ -0,0 +1,18 @@
+package frc.robot.sub;
+import edu.wpi.first.math.controller.PIDController;
+
+public class Modifiers {
+    
+    public PIDController PIDcontrollermath;
+
+    public double speed = 0.5;
+    public double direction =  0;
+    public double distance = 0;
+    
+    public void PIDvalues()
+    {
+        double setPoint = 0.0;
+        PIDcontrollermath.getIntergratorRange(-1000, 1000);
+        PIDcontrollermath.getPositionTolerance();
+    }
+}
