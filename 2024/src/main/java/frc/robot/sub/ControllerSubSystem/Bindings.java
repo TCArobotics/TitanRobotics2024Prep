c9cd9e66ef262e50f5bf816461664f77d9e21df1
SwissCheez84 <JustResolve@outlook.com>
2024-01-25 16:00:43 -0800

    controller stuff
    cleaned up old controller system and put groundwork for new one

--- 2024/src/main/java/frc/robot/sub/ControllerSubSystem/Bindings.java
+++ 2024/src/main/java/frc/robot/sub/ControllerSubSystem/Bindings.java
@@ -1,3 +1,67 @@
-public enum Bindings {
-    //WHY IM SO CONFUSED
+package frc.robot.sub;
+
+import frc.robot.data.ButtonMap;
+
+public class Bindings {
+	static final pressed = 1;
+	static final held = 2;
+	// to get if a button is pressed (controllerState[index] & pressed != 0)
+
+	XboxController xboxController;
+	int[] xboxControllerProfile = {
+		 1, //A
+		 2, //B
+		 3, //X
+		 4, //Y
+		 5, //LB
+		 6, //RB
+		 7, //BACK
+		 8, //START
+		 9, //LSTICKPUSH
+		-5, //LSTICKX
+		-6, //LSTICKY
+		10, //RSTICKPUSH
+		-7, //RSTICKX
+		-8  //RSTICKY
+	};
+
+	xboxController flightStickController;
+	int[] flightStickControllerProfile = {
+		 1, //BUTTON 1
+		 2, //BUTTON 2
+		 3, //BUTTON 3
+		 4, //BUTTON 4
+		 5, //BUTTON 5
+		 6, //BUTTON 6
+		 7, //BUTTON 7
+		 8, //BUTTON 8
+		 9, //BUTTON 9
+		10, //BUTTON 10
+		11, //BUTTON 11
+		12, //BUTTON 12
+		-1, //ROLL
+		-2, //PITCH
+		-3, //YAW
+		-4  //SLIDER
+	};
+
+	int[] controllerState = new int[30];
+
+	private void updateControllerState(int[] profile, int index, XboxController controller) {
+		for (int i = 0; i < profile.length; i++) {
+			if (profile[i] < 0) { // axis
+				controllerState[i + index] = controller.getRawAxis(profile[i]);
+			} else {              // button
+				if (controller.getRawButton(profile[i])) {
+					if (controllerState[i + index] == 0) {
+						controllerState[i + index] = 3; // bin (11)
+					} else {
+						controllerState[i + index] = 2; // bin (10)
+					}
+				} else {
+					controllerState[i + index] = 0;
+				}
+			}
+		}
+	}
 }
--- 2024/src/main/java/frc/robot/sub/Controllers.java
+++ 2024/src/main/java/frc/robot/sub/Controllers.java
@@ -8,17 +8,17 @@ import java.util.HashMap;
 import edu.wpi.first.wpilibj.Timer;
 
 public class Controllers { // may whant to rewright some time later 7-26-2023
-    private XboxController xboxController; 
-    
+    private XboxController xboxController;
+
     private HashMap<ButtonMap, Double> buttons;
     private double debouncePeriod = 0.1; //The time before a button is allowed to be pressed again in seconds
     public Controllers()
     {
-        try 
+        try
         {
             this.xboxController = new XboxController(PortMap.GAMEPADXBOX.portNumber);
         }
-        catch (Exception xboxControllerNotAssigned) 
+        catch (Exception xboxControllerNotAssigned)
         {
             this.xboxController = null;
             SmartDashboard.putNumber("XboxControllerNotFound", PortMap.GAMEPADXBOX.portNumber);
@@ -26,7 +26,7 @@ public class Controllers { // may whant to rewright some time later 7-26-2023
         this.buttons = new HashMap<ButtonMap, Double>();
         init();
     }
-    
+
     private void init()
     {
         for(ButtonMap i : ButtonMap.values())
@@ -36,9 +36,9 @@ public class Controllers { // may whant to rewright some time later 7-26-2023
     }
 
     /**Input the ButtonMap name and receive if button is pressed, boolean true or false; has debounce (time before button can output true again) */
-    public boolean getButtonXboxPressed(ButtonMap buttonName) 
+    public boolean getButtonXboxPressed(ButtonMap buttonName)
     {
-    
+
         double currentTime = Timer.getFPGATimestamp();
         if(currentTime - buttons.get(buttonName) > this.debouncePeriod && this.xboxController != null)
         {
@@ -50,21 +50,21 @@ public class Controllers { // may whant to rewright some time later 7-26-2023
             return false;
         }
     }
-    
+
     /**Input the ButtonMap name and receive if button is pressed, boolean true or false; does not have debounce (allows for motors to be triggered by press and hold until button is released) */
-    public boolean getButtonXboxPressedDebounceOff(ButtonMap buttonName) 
+    public boolean getButtonXboxPressedDebounceOff(ButtonMap buttonName)
     {   if (this.xboxController != null)
         {
             return xboxController.getRawButton(buttonName.value);
         }
-        else 
+        else
         {
             return false;
         }
     }
 
     /**Input the ButtonMap name and axis and receive its value, double between -1 and 1 */
-    public double getStick(ButtonMap stickAxis) 
+    public double getStick(ButtonMap stickAxis)
     {  if (this.xboxController != null)
         {   try {
             switch(stickAxis)
@@ -74,23 +74,23 @@ public class Controllers { // may whant to rewright some time later 7-26-2023
                 case XboxLEFTSTICKY:
                     return xboxController.getRawAxis(1);
                 case XboxRIGHTSTICKX:
-                    return xboxController.getRawAxis(4); 
+                    return xboxController.getRawAxis(4);
                 case XboxRIGHTSTICKY:
-                    return xboxController.getRawAxis(5); 
+                    return xboxController.getRawAxis(5);
                 default:
                     return 0;
             }
-        }  
-        catch (Exception AxisNotFound) 
+        }
+        catch (Exception AxisNotFound)
         {
             SmartDashboard.putString("ControllerError", "AxisNotFound");
             return 0;
         }
-        } 
-        else 
+        }
+        else
         {
         return 0;
-        }     
+        }
     }
 
 }
